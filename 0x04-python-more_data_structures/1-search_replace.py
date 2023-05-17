706d355fc13f978b7931a8e542a3ccf4b13e2e0e
Chioma Igwe <cigwe072013@gmail.com>
2023-05-17 08:06:23 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,8 @@
 
 def search_replace(my_list, search, replace):
     # Create a new list with the same elements as the input list
-    new_list = [replace if item == search else item for item in my_list]
-    return new_list
+    new_list = my_list[:]
+    for n in range(len(new_list)):
+        if new_list[n] == search:
+            new_list[n] = replace
+    return (new_list)
