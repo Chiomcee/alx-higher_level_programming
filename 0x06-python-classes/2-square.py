459efdba00fa6ab822420d40594cb79660ef2696
Chioma Igwe <cigwe072013@gmail.com>
2023-05-30 16:01:07 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Describes a class square"""
+
+    def __init__(self, size=0):
+        """initializes a square class
+        Args:
+        TypeError: if size is not an integer
+        Valueerror: if size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
