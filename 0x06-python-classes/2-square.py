28b770c90bd63fd34a34bc772d6398320076a615
Chioma Igwe <cigwe072013@gmail.com>
2023-05-30 20:36:02 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-# 0-square.py by Chioma Igwe
-"""Defines a square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Describes a class Square"""
+    """Describes a square."""
 
     def __init__(self, size=0):
-        """Initializes this square class
-        Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """Initializing a new Square.
 
+        Args:
+            size (int): Represent size of the new square.
+       """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
