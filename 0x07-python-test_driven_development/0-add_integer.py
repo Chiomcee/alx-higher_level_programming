15a09bd1f5a0a516c336795de63a168085bb7a3f
Chioma Igwe <cigwe072013@gmail.com>
2023-06-03 17:05:20 -0700

    README.md

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+
+"""Method that defines addition of two intergers"""
+
+def add_integer(a, b=98):
+    """Returns addition of integer""""
+    if not istance
