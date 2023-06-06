b071ac24c898c9cc3fdcbb82e8b7d6bd7ea065c8
Chioma Igwe <cigwe072013@gmail.com>
2023-06-06 14:42:31 -0700

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a locked class
+"""
+
+
+class LockedClass:
+    """
+    The class LockedClass prevents the user from dynamically creating new instance attributes except for first_name
+    """
+
+    __slots__ = ["first_name"]
