4f6804e6f00c9835513fcb9ff78437e3e772e71d
Chioma Igwe <cigwe072013@gmail.com>
2023-06-12 05:22:13 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Represents a base geometry"""
+
+    def area(self):
+        """method yet to be implemented"""
+        raise Exception("area() is not implemented")
