55a4b1c315099bca470d8becc19691ff09f00c7c
Chioma Igwe <cigwe072013@gmail.com>
2023-06-13 19:48:44 -0700

    creates object from a JSOn file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
-#!/uisr/bin/python3
-"""Module that converts a JSON file to python object"""
+#!/usr/bin/python3
+"""This module defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
-    """function that creates an object from JSON file"""
+    """function that creates an object from a JSON file"""
     with open(filename) as f:
         return json.load(f)
