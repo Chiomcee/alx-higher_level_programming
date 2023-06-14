3dfe185111cf0265b134717c1c899aa3116e4245
Chioma Igwe <cigwe072013@gmail.com>
2023-06-13 21:54:08 -0700

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module defines a text file insertion function"""
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file, after each line containing a specific string"""
+    with open(filename, mode='r+', encoding='utf-8') as f:
+        lines = f.readlines()  # Read all lines of the file
+        f.seek(0)  # Reset the file pointer to the beginning of the file
+        for line in lines:
+            f.write(line)  # Write the line to the file
+            if search_string in line:
+                f.write(new_string)  # Write the new string after the line if it contains the search string
+        f.truncate()  # Truncate the file to remove any remaining content
