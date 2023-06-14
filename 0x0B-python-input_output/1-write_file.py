ffcd3cf3cb0b770e9b9e21a5cb65ffa575f5537f
Chioma Igwe <cigwe072013@gmail.com>
2023-06-13 17:58:17 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that creates a UTF-8 txt file and write a string to it"""
 
+
 def write_file(filename="", text=""):
     """creates a text file and write a string to it."""
     with open(filename, "w", encoding="utf-8") as f:
