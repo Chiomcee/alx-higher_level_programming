09f9b6de023a0f2a1389ba00a914f6750ba6595c
Chiomcee <cigwe072013@gmail.com>
2023-06-20 17:53:43 +0100

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,13 +3,14 @@
 
 import json
 
+
 class Base:
     """A class representing base of all classes created"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """function class constructor""" 
+        """function class constructor"""
         if id is not None:
             self.id = id
         else:
@@ -25,7 +26,10 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Class method that writes the JSON string representation of list_objs to a file"""
+        """
+        Class method that writes the JSON string representation
+        of list_objs to a file
+        """
         filename = cls.__name__ + ".json"
         list_dicts = []
         if list_objs is not None:
