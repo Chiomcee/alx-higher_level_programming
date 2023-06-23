1a20e9b4d575cf5cf99971b4a31b351f4eafa4f0
Chiomcee <cigwe072013@gmail.com>
2023-06-23 03:59:35 +0100

    doc strin attached for dic

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,4 +122,5 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
         return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
